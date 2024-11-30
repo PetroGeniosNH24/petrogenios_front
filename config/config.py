0597a7ff4d1b3deae9132eacbb6776e3931f6677
ivan.corral_bluetab <ivan.corral.m@servexternos.repsol.com>
2024-11-30 14:23:05 +0100

    add url deploy back

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-URL_BACKEND = "http://127.0.0.1:5000"
+URL_BACKEND = "https://test-streamlit-back-dfapbufedthng0f8.uksouth-01.azurewebsites.net"
 HEADER_DATABRICK = {
     "Authorization": "Bearer dapi31dbb54f088ab12d65d930ce546bd3d0",  # Aquí va tu token personal
     "Content-Type": "application/json"
